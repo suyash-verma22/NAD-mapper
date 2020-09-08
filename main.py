2b487290eae3bed74b8c98ab91317537d8dae8e1
verma.suyash22 <verma.suyash22@gmail.com>
2020-09-08 13:57:21 +0530

    latest working

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from flask import *
 import pandas as pd
 from werkzeug.exceptions import HTTPException
-from standard_columns import std_col,checkMandatoryFields,columnSequencing
+from standard_columns import std_col,checkMandatoryFields,columnSequencing,randomName
+from standard_columns import dbf_to_csv
 from datetime import datetime
-#from mapper_forms import MapperForm
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
 
-std_colName  = std_col              #standard colums names
-filename = ""                       #to store name of input file
-p_colName = []                      #list to add columns of input csv file
-input_file = ""                     #variable to store name of input csv file
+app.config['MAX_CONTENT_LENGTH'] = 50*1024        # setting a limit on file size
+
+std_colName  = std_col                            #standard colums names
+filename = ""                                     #to store name of input file
+p_colName = []                                    #list to add columns of input csv file
+input_file = ""                                   #variable to store name of input csv file
+
+
+
+#######################################
+################ HOME #################
+#######################################
 
 @app.route("/")
 @app.route("/home")
@@ -18,7 +28,10 @@ def home():
     return render_template("home.html")
 
 
-#################################################################################################################################
+#######################################
+######## Column Mapper Page ###########
+#######################################
+
 @app.route("/map_col",methods = ['GET','POST'])
 def map_col():
     global std_colName
@@ -31,45 +44,63 @@ def map_col():
         for col in input_file.columns:
              p_colName.append(col)
         return render_template('update_col.html',cols = p_colName,std_cols = std_col)
+    elif file_extension == "DBF":
+        file_input = dbf_to_csv(filename)
+        input_file = pd.read_csv(file_input) 
+        for col in input_file.columns:
+             p_colName.append(col)
+        return render_template('update_col.html',cols = p_colName,std_cols = std_col)
     else:
         return render_template('wrongFileType.html',file_extension=file_extension)
-#####################################################################################################################################
-save_state = {} # dictionary to the states of mapped columns
+
+
+#######################################
+######## File Downloaded Page #########
+#######################################
+
+save_state = {}                                # dictionary to save state of mapped columns
 col_list = []
-#temp_col_list = []
 @app.route("/download_csv",methods = ['GET','POST'])
 def download_csv():
     global col_list
     global filename
-    #global temp_col_list
-    #temp_col_list = col_list
     col_list = request.form.getlist('col')
     i=0
     for each_col in input_file:
-        save_state[each_col] = col_list[i]
+        save_state[each_col] = col_list[i]     # storing current mapping to dictionary
         i+=1
     temp = checkMandatoryFields(col_list)      # check mandotory fields
     if temp == "true":
         col_list = columnSequencing(col_list)
-        #temp_name = "" + datetime.now() +".csv"
+        name = randomName()
         file = pd.read_csv(filename,header=None,skiprows=1,names=col_list)
-        file.to_csv("output.csv",index=False)
-        return render_template("download.html")
+        file.to_csv(name,index=False)
+        return render_template("download.html",name=name)
     else:
          return render_template("update_col.html",cols = col_list,std_cols = std_col,error=temp,state=save_state)
         
-##########################################################################################################################################################
-# 403 FORBIDDEN
+
+######################################
+############### 403 ##################
+######################################
+
 @app.errorhandler(403)
 def forbidden(e):
     return render_template("forbidden.html"),404
 
-# 404 PAGE NOT FOUND
+######################################
+############### 404 ##################
+######################################
+
 @app.errorhandler(404)
 def page_not_found(e):
     app.logger.info(f"Page not found: {request.url}")
     return render_template("404.html"),404
 
 
+######################################
+############### Main ##################
+######################################
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- standard_columns.py
+++ standard_columns.py
@@ -1,3 +1,7 @@
+import random
+import string
+import csv
+from dbfread import DBF
 
 ###############################
 ####### Standard Columns ######
@@ -6,36 +10,44 @@
 
 col = "SCH_CODE_AI	SCH_NAME	SCH_NAME_L	SCH_PIN	ORG_ADDRESS	ORG_CITY	ORG_STATE	ORG_PIN	ACADEMIC_COURSE_ID	COURSE_NAME	COURSE_NAME_L	STREAM	STREAM_L	SESSION	REGN_NO	RROLL	AADHAAR_NO	CNAME	GENDER	DOB	CASTE	RELIGION	NATIONALITY	PH	MOBILE	FNAME	MNAME	GNAME	PHOTO	MRKS_REC_STATUS	SIGN	RESULT	YEAR	MONTH	DIVISION	GRADE	PERCENT	DOI	CERT_NO	SUB1NM	SUB1	SUB1MAX	SUB1MIN	SUB1_TH_MAX	SUB1_TH_MIN	SUB1_PR_MAX	SUB1_PR_MIN	SUB1_TH_MRKS	SUB1_PR_MRKS	SUB1_TOT	SUB1_STATUS	SUB1_GRADE	SUB1_GRACE	SEM	EXAM_TYPE	TOT	TOT_MRKS	TOT_MRKS_WRDS	CGPA	SGPA	SUB2NM	SUB2	SUB2MAX	SUB2MIN	SUB2_TH_MAX	SUB2_TH_MIN	SUB2_PR_MAX	SUB2_PR_MIN	SUB2_TH_MRKS	SUB2_PR_MRKS	SUB2_TOT	SUB2_STATUS	SUB2_GRADE	SUB2_GRACE	SUB3NM	SUB3	SUB3MAX	SUB3MIN	SUB3_TH_MAX	SUB3_TH_MIN	SUB3_PR_MAX	SUB3_PR_MIN	SUB3_TH_MRKS	SUB3_PR_MRKS	SUB3_TOT	SUB3_STATUS	SUB3_GRADE	SUB3_GRACE	SUB4NM	SUB4	SUB4MAX	SUB4MIN	SUB4_TH_MAX	SUB4_TH_MIN	SUB4_PR_MAX	SUB4_PR_MIN	SUB4_TH_MRKS	SUB4_PR_MRKS	SUB4_TOT	SUB4_STATUS	SUB4_GRADE	SUB4_GRACE	SUB5NM	SUB5	SUB5MAX	SUB5MIN	SUB5_TH_MAX	SUB5_TH_MIN	SUB5_PR_MAX	SUB5_PR_MIN	SUB5_TH_MRKS	SUB5_PR_MRKS	SUB5_TOT	SUB5_STATUS	SUB5_GRADE	SUB5_GRACE"
 
-std_col = col.split() #list of standard fields
+std_col = col.split()                       #list of standard fields
 
-mandatory_cols = ["CNAME","FNAME","MNAME","RROLL","YEAR","REGN_NO"]
+mandatory_cols = ["CNAME","FNAME","MNAME","RROLL","YEAR","REGN_NO"]  # mandatory columns
 
-def checkMandatoryFields(colp):
-
-    if mandatory_cols[0] not in colp:
+def checkMandatoryFields(colp):             # function to check that all the mandatory
+    if mandatory_cols[0] not in colp:       # feilds are mapped or not
         return mandatory_cols[0]
-
     if mandatory_cols[1] not in colp:
         return mandatory_cols[1]
-
     if mandatory_cols[2] not in colp:
         return mandatory_cols[2]
-
     if mandatory_cols[3] not in colp:
         return mandatory_cols[3]
-
     if mandatory_cols[4] not in colp:
         return mandatory_cols[4]
-
     if mandatory_cols[5] not in colp:
         return mandatory_cols[5]
-
     return "true"
 
 
-def columnSequencing(col_list):
+def columnSequencing(col_list):             # function to check the column sequencing
     temp = std_col
     for c in col_list:
         if c not in temp:
             temp.append(c)
         return temp    
+
+
+def randomName():
+    return ''.join(random.choices(string.ascii_uppercase +string.digits, k = 10))+".csv"
+
+def dbf_to_csv(dbf_table_pth):
+    csv_fn = dbf_table_pth[:-4]+ ".csv" 
+    table = DBF(dbf_table_pth)
+    with open(csv_fn, 'w', newline = '') as f:
+        writer = csv.writer(f)
+        writer.writerow(table.field_names)
+        for record in table:
+            writer.writerow(list(record.values()))
+    return csv_fn
+
